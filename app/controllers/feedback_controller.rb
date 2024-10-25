2b4b61c82e84e0432bc7bd0253896ea630de91d2
Dan Ivovich <divovich@gmail.com>
2024-10-25 10:54:20 -0400

    Update captcha and add to signup

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -16,7 +16,7 @@ class FeedbackController < ApplicationController
   end
 
   def recaptcha_success
-    success = verify_recaptcha(model: @tip, minimum_score: 0.7, action: 'submit_tip', secret_key: ENV['RECAPTCHA_SECRET_KEY_V3'])
+    success = verify_recaptcha(minimum_score: 0.9, action: 'submit_feedback', secret_key: ENV['RECAPTCHA_SECRET_KEY_V3'])
     checkbox_success = verify_recaptcha unless success
     if success || checkbox_success
       true
