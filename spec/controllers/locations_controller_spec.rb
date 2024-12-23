295f3d763e0265f57809db6ecd4a62651add67ee
Micaela Cunha <micaela@smartlogic.io>
2024-12-23 18:35:08 -0500

    BCHDCC-100 Fixed tests to look for Location Visit events that match a given location id

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -4,7 +4,6 @@ describe LocationsController do
   # create location with service under the 'Health' category
   before do
     @nearby = create(:nearby_loc)
-    @new_test_location = create(:farmers_market_loc)
 
     health = create(:health)
 
@@ -23,34 +22,31 @@ describe LocationsController do
     end
   end
 
-  context "tracking ahoy visits with ahoy" do
-    before(:each){ Ahoy::Event.delete_all }
+  describe "track single location visits with ahoy" do
+    it 'tracks the visited location id' do
+      get :show, params: {id: "#{@nearby.slug}"}
 
-    describe "track single location visits with ahoy" do
-      it 'tracks the visited location id' do
-        get :show, params: {id: "#{@nearby.slug}"}
+      ahoy_tracked = Ahoy::Event.where(name: 'Location Visit', properties: {id: @nearby.id}).count
 
-        ahoy_tracked = Ahoy::Event.where(name: 'Location Visit', id: @nearby.id).count
-
-        expect(ahoy_tracked).to eq(1)
-      end
+      expect(ahoy_tracked).to eq(1)
     end
+  end
 
-    describe "track multiple location visits with ahoy" do
-      it 'tracks multiple location visits in the same ahoy visit id' do
-        get :show, params: {id: "#{@nearby.slug}"}
-        get :show, params: {id: "#{@new_test_location.slug}"}
+  describe "track multiple location visits with ahoy" do
+    it 'tracks multiple location visits in the same ahoy visit id' do
+      get :show, params: {id: "#{@nearby.slug}"}
+      @new_test_location = create(:farmers_market_loc)
+      get :show, params: {id: "#{@new_test_location.slug}"}
 
-        ahoy_events = Ahoy::Event.where(name: 'Location Visit')
-        nearby_loc_tracked = Ahoy::Event.where(name: 'Location Visit', id: @nearby.id)
-        new_loc_tracked = Ahoy::Event.where(name: 'Location Visit', id: @new_test_location.id)
+      ahoy_events = Ahoy::Event.where(name: 'Location Visit')
+      nearby_loc_tracked = Ahoy::Event.where(name: 'Location Visit', properties: {id: @nearby.id})
+      new_loc_tracked = Ahoy::Event.where(name: 'Location Visit', properties: {id: @new_test_location.id})
 
-        expect(ahoy_events.count).to eq(2)
-        expect(ahoy_events.first.properties["id"]).to eq(@nearby.id)
-        expect(nearby_loc_tracked.count).to eq(1)
-        expect(new_loc_tracked.count).to eq(1)
-        expect(ahoy_events.first.visit_id).to eq(ahoy_events.last.visit_id)
-      end
+      expect(ahoy_events.count).to eq(2)
+      expect(ahoy_events.first.properties["id"]).to eq(@nearby.id)
+      expect(nearby_loc_tracked.count).to eq(1)
+      expect(new_loc_tracked.count).to eq(1)
+      expect(ahoy_events.first.visit_id).to eq(ahoy_events.last.visit_id)
     end
   end
 end
