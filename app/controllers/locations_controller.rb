49e25fd0e0184c86f9ec694476813bbabc73d5cf
Micaela Cunha <micaela@smartlogic.io>
2024-12-30 12:11:43 -0500

    BCHDCC-107 Moved ahoy tracking of performed searches from locations controller to helper file

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -50,11 +50,7 @@ class LocationsController < ApplicationController
     @exact_match_found = @locations_search.exact_match_found?
 
     # tracks info about the current search
-    ahoy.track( "Perform Search",
-                keywords: params[:keyword],
-                main_category: @main_category_selected_name,
-                subcategories: @selected_categories,
-                results: @search.locations.total_count)
+    fire_perform_search_event
 
     # caches the search results and renders the view
     cache_page(@search.locations) if @search.locations.present?
